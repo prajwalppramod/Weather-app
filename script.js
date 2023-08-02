a14c730167e927508bf33266c41d8c0c296434f7
Prajwal P Pramod <98303757+prajwalppramod@users.noreply.github.com>
2023-08-02 19:07:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,41 +5,52 @@ const locationElement = document.querySelector('.location h2');
 const tempElement = document.querySelector('.temp');
 const conditionElement = document.querySelector('.condition');
 
-// Replace with your OpenWeatherMap API key
-const apiKey = 'c03416c3e4d4842277448d96c6543ce7';
+// Your OpenWeatherMap API key
+const apiKey = '0fc077cd6cf3989d0a8c2146560e82cb';
 
 // Function to fetch weather data from OpenWeatherMap API
 async function getWeather(city) {
-    try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-      
-      if (!response.ok) {
-        throw new Error(`API request failed with status: ${response.status} ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-      return null;
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`);
+    
+    if (!response.ok) {
+      throw new Error(`API request failed with status: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    return null;
+  }
+}
+
+// Function to update the UI with weather data
+function updateWeatherUI(data) {
+  if (data) {
+    locationElement.textContent = `${data.name}, ${data.sys.country}`;
+    tempElement.textContent = `${Math.round(data.main.temp)}°C`;
+    conditionElement.textContent = data.weather[0].main;
+  } else {
+    locationElement.textContent = 'City not found';
+    tempElement.textContent = '';
+    conditionElement.textContent = '';
+  }
+}
+
+// Event listener for the search button
+searchButton.addEventListener('click', async () => {
+  const city = cityInput.value.trim();
+  if (city) {
+    const weatherData = await getWeather(city);
+
+    if (weatherData) {
+      updateWeatherUI(weatherData);
+    } else {
+      updateWeatherUI(null);
     }
   }
-  
-  
-  // Event listener for the search button
-  searchButton.addEventListener('click', async () => {
-    const city = cityInput.value.trim();
-    if (city) {
-      const weatherData = await getWeather(city);
-  
-      if (weatherData) {
-        updateWeatherUI(weatherData);
-      } else {
-        updateWeatherUI(null);
-      }
-    }
-  });
-  
-  // Initial UI update (optional)
-  updateWeatherUI(null);
-  
+});
+
+// Initial UI update (optional)
+updateWeatherUI(null);
